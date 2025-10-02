8e29fad0d4326ce7e81388457594e05b5c393338
balakrishnasurya <90981476+balakrishnasurya@users.noreply.github.com>
2025-10-02 19:51:17 +0530

    "trying"

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
-    // Disable LightningCSS (use PostCSS instead)
+    // Disable LightningCSS (this forces PostCSS fallback)
     optimizeCss: false,
+    optimizeFonts: false
   },
+  swcMinify: false, // Optional: sometimes SWC minifier triggers LightningCSS
 };
 
 module.exports = nextConfig;
